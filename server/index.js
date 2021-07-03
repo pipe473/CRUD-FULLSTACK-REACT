12462ced54fa35ce229794df5db62c42ee998672
pipe473 <bedoyafelipe743@gmail.com>
2021-07-03 19:56:15 +0200

    Modifications in post method

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const mysql = require('mysql');
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const mysql = require("mysql");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 let PORT = 6060;
 let app = express();
@@ -11,98 +11,106 @@ app.use(bodyParser.json());
 app.use(cors());
 
 let mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    port: 8889,
-    user: 'root',
-    password: 'root',
-    database: 'EmployeeDB',
-    multipleStatements: true
+  host: "localhost",
+  port: 8889,
+  user: "root",
+  password: "root",
+  database: "EmployeeDB",
+  multipleStatements: true,
 });
 
-mysqlConnection.connect((err) =>{
-    if(!err)
-        console.log('DB COnnection succeded...');  
-    else
-        console.log('DB connection failed! \n Error:'+ JSON.stringify(err, undefined, 2));      
+mysqlConnection.connect((err) => {
+  if (!err) console.log("DB COnnection succeded...");
+  else
+    console.log(
+      "DB connection failed! \n Error:" + JSON.stringify(err, undefined, 2)
+    );
 });
-
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-
 // Get all employees from DB
 
-app.get('/employees', (req, res) =>{
-    mysqlConnection.query('SELECT * FROM Employee', (err, rows, fields) =>{
-        if (error) throw error;
-        if(!err)
-            res.send(rows);
-        else
-            console.log(err);       
-    });
+app.get("/employees", (req, res) => {
+  let employResults = [req.body];
+  let mySql = "SELECT * FROM Employee";
+  mysqlConnection.query(
+      mySql,
+      employResults,
+       (err, response) => {
+    if (!err) {
+      res.send(response);
+    } else {
+      console.log(err);
+    }
+  });
 });
 
 // Get an employee by id from DB
 
-app.get('/employees/:id', (req, res) =>{
-    mysqlConnection.query('SELECT * FROM Employee WHERE EmpID = ?', [req.params.id], (err, rows, fields) =>{
-        if (error) throw error;
-        if(!err)
-            res.send(rows);
-        else
-            console.log(err);       
-    });
+app.get("/employees/:id", (req, res) => {
+  mysqlConnection.query(
+    "SELECT * FROM Employee WHERE EmpID = ?",
+    [req.params.id],
+    (err, rows, fields) => {
+      if (error) throw error;
+      if (!err) res.send(rows);
+      else console.log(err);
+    }
+  );
 });
 
 // Delete an employee by id from DB
 
-app.delete('/employees/:id', (req, res) =>{
-    mysqlConnection.query('DELETE FROM Employee WHERE EmpID = ?', [req.params.id], (err, rows, fields) =>{
-        if(!err)
-            res.send('Deleted succesfully!');
-        else
-            console.log(err);       
-    });
+app.delete("/employees/:id", (req, res) => {
+  mysqlConnection.query(
+    "DELETE FROM Employee WHERE EmpID = ?",
+    [req.params.id],
+    (err, rows, fields) => {
+      if (!err) res.send("Deleted succesfully!");
+      else console.log(err);
+    }
+  );
 });
 
 // Insert an employee by id from DB
 
-app.post('/employees', (req, res) =>{
-    let emParams = [
-        req.body.EmpID,
-        req.body.Name,
-        req.body.EmpCode,
-        req.body.Salary
-    ];
-   
-    let mySql = "INSERT INTO employee(EmpID,Name,EmpCode,Salary) VALUES (?,?,?,?)";
-    mysqlConnection.query(
-        mySql,
-        emParams, (err, response) => {
-            if (!err) {
-                console.log(err);                
-            } else {
-                res.send(response, "");
-            }
-    });
+app.post("/employees", (req, res) => {
+  let emParams = [
+    req.body.EmpID,
+    req.body.Name,
+    req.body.EmpCode,
+    req.body.Salary,
+  ];
+
+  let mySql = "INSERT INTO employee(EmpID,Name,EmpCode,Salary) VALUES (?,?,?,?)";
+  mysqlConnection.query(
+      mySql,
+      emParams, (err, response) => {
+    if (!err) {
+      res.send(response);
+    } else {
+      console.log(err);
+    }
+  });
 });
 
 // Update an employee by id from DB
 
-app.put('/employees', (req, res) =>{
-    let emParams = [
-        req.body.Name,
-        req.body.EmpCode,
-        req.body.Salary,
-        req.body.EmpID,
-    ];
-    let sql = "UPDATE employee SET Name = ?, EmpCode = ?, Salary = ? WHERE  EmpID = ?";
-    mysqlConnection.query(sql,emParams, (err, rows, fields) =>{
-        if(!err){
-           res.send("Updated successfully!!")
-        }        
-        else{
-            console.log(err);       
-        }
-    });
+app.put("/employees", (req, res) => {
+  let emParams = [
+    req.body.Name,
+    req.body.EmpCode,
+    req.body.Salary,
+    req.body.EmpID,
+  ];
+  let sql =
+    "UPDATE employee SET Name = ?, EmpCode = ?, Salary = ? WHERE  EmpID = ?";
+  mysqlConnection.query(sql, emParams, (err, rows, fields) => {
+    if (!err) {
+      res.send("Updated successfully!!");
+    } else {
+      console.log(err);
+    }
+  });
 });
